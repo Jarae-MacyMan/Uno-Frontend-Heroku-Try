4676efe1dfa060528985b5694f087697e8cd643c
Khalia Parris <khaliaparris01@gmail.com>
2022-07-26 14:43:00 -0400

    started a react app

---
+++ hot-potato/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        Hot Potato
+      </header>
+      <button>click me</button>
+     
+    </div>
+  );
+}
+
+export default App;
